a5a8a4b2230ce5a2090dcc35e8fb6cac25d7edd0
Miriam Ahlberg <tna12mah@student.lu.se>
2017-12-02 21:13:17 +0100

    Get the ids for the videos

--- download_playlist.py
+++ download_playlist.py
@@ -1,6 +1,18 @@
 from pytube import YouTube
 from bs4 import BeautifulSoup as bs
 import requests
+from urllib.parse import urlparse, parse_qs
+
+
+def get_id(url):
+    u_pars = urlparse(url)
+    quer_v = parse_qs(u_pars.query).get('v')
+    if quer_v:
+        return quer_v[0]
+    pth = u_pars.path.split('/')
+    if pth:
+        return pth[-1]
+
 
 playlistLink = 'https://www.youtube.com/playlist?list=PLql1guR0G27vdWY8EM6E8mYWIZhpRrefz'
 
@@ -10,8 +22,7 @@ soup = bs(page,'html.parser')
 res = soup.find_all('a', {'class': 'pl-video-title-link'})
 
 urls = list(map(lambda x: 'https://www.youtube.com' + x.get("href"), res))
-
-print('STARTING')
+ids = list(map(lambda x: get_id(x), urls))
 
 for url in urls:
     print('Downloading   '+url)
